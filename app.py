807137b960bb5060016fdf4a18fac544efd29195
0xgmcarl <126638250+crafty-arl@users.noreply.github.com>
2024-10-03 13:25:00 -0500

    at home

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ from dashboard import show_dashboard
 from account import show_account
 from assessments import show_assessments
 from food_journal import show_food_journal
+import os
+from supabase import create_client, Client
+from postgrest import APIError
+
+API_URL: str = os.environ.get("SUPABASE_URL")
+API_KEY: str = os.environ.get("SUPABASE_KEY")
+supabase_client: Client = create_client(API_URL, API_KEY)
+
+st.write(supabase_client)
 
 ui.avatar(src="https://your_image_url")
 st.write("Welcome User name")
@@ -18,3 +27,24 @@ elif selected_tab == 'Assessments':
     show_assessments()
 elif selected_tab == 'Food Journal':
     show_food_journal()
+
+if st.button('Insert Data'):
+    try:
+        response = (
+            supabase_client.table("table_name")
+            .insert(
+                [
+                    {
+                        "id": 1,
+                        "Date": "Denmark",
+                        "Race": "1",
+                        "Track": "Mario Kart Stadium",
+                        "Finish": "1",
+                    }
+                ]
+            )
+            .execute()
+        )
+        st.write("Data inserted successfully")
+    except APIError as e:
+        st.error(f"An error occurred: {e}")
